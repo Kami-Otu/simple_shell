aa204723a595c9c5ce142b2b1600829257b7621f
Divine Ekeh <divineekeh6@gmail.com>
2023-05-24 06:01:22 -0700

    memory.c

---
+++ memory.c
@@ -0,0 +1,18 @@
+#include "alxse.h"
+
+/**
+ * bfree - func that frees a pointer and NULLs the address
+ * @ptr: the address of the pointer to free
+ *
+ * Return: (1) if freed, otherwise (0).
+*/
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+{
+	free(*ptr);
+	*ptr = NULL;
+	return (1);
+}
+	return (0);
+}
